7d91a4628164e0cb879619cda1b6b392c76d8e07
n2jn <nicolas.dejean8@gmail.com>
2025-12-05 17:18:55 +0100

    chore: started mobile integration

--- apps/backend/dist/auth/strategies/jwt.strategy.d.ts
+++ apps/backend/dist/auth/strategies/jwt.strategy.d.ts
@@ -5,14 +5,6 @@ declare const JwtStrategy_base: new (...args: any[]) => Strategy;
 export declare class JwtStrategy extends JwtStrategy_base {
     private usersService;
     constructor(configService: ConfigService, usersService: UsersService);
-    validate(payload: any): Promise<{
-        email: string;
-        username: string;
-        role: import("@generated/prisma").$Enums.UserRole;
-        id: string;
-        name: string;
-        createdAt: Date;
-        updatedAt: Date;
-    } | null>;
+    validate(payload: any): Promise<any>;
 }
 export {};
